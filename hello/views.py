a34d4b8a50dbcd1583cb4598d885863691ce330c
kiyohiro kawamura <kyorohiro@gmail.com>
2019-01-04 23:14:31 +0900

    sc

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,27 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Message
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    latest_question_list = Message.objects.order_by('created')[:5]
+    print("==========")
+    print(latest_question_list)
+    print("==========")
+
+    template = loader.get_template('index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse("Hello, Django!")
 
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
